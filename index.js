3c65a9adb5cfa73af648851cd737d795a5047a3a
diegofertr <diegonano09@gmail.com>
2016-12-19 18:34:36 -0400

    Añadiendo carpeta socket.io

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const express = require('express');
 const socketIO = require('socket.io');
 const r = require('rethinkdb');
 const config = require('./config.json');
- 
+
 // Loading Express, HTTP, Socket.IO and RethinkDB
 const db = Object.assign(config.rethinkdb, { db: 'timeline' });
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
- 
+
 // Index route which renders the index.html
 app.get('/', (req, res) => {
   fs.readFile(`${__dirname}/index.html`, (err, html) => {
     res.end(html || err);
   });
 });
- 
+
 // The changefeed is provided by change() function
 // which emits broadcast of new messages for all clients
 r.connect(db)
@@ -29,7 +29,7 @@ r.connect(db)
     });
   })
 ;
- 
+
 // Listing all messages when new user connects into socket.io
 io.on('connection', (client) => {
   r.connect(db)
@@ -55,4 +55,3 @@ server.listen(3000, () => console.log('Timeline Server!'));
 // ver los datos de la tabla messages
 // r.db('timeline').table('messages')
 // añadi este comentario a manera de que no haya conflictos con el codigo, aun no soy un experto en git.
-
